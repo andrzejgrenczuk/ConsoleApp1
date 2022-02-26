eeaa1421b5c1c90634b4d551af58e0f2d548eee8
Andrzej Greńczuk <andrzej.grenczuk@gmail.com>
2022-02-27 00:02:48 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.Write("wpisz coś: ");
+            string text = Console.ReadLine();
+            Console.Write("wpisano: "+text);
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------");
+            DateTime dat = DateTime.Now;
+            Console.WriteLine("Today is: " + dat);
+            Console.WriteLine("\nToday is {0:d} at {0:T}.", dat);
+
+        }
+    }
+}
